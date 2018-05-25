183ee805ba5060c87cd77cf81cbb2acb3bfde189
谭俊 <tanj@csdas.cn>
2018-05-25 11:17:52 +0800

    for 写法错误

--- Spider/test/get_date.py
+++ Spider/test/get_date.py
@@ -4,6 +4,6 @@ from bs4 import BeautifulSoup
 html = urlopen('http://jr.jd.com')
 bs_obj = BeautifulSoup(html.read(),'html.parser')
 text_list = bs_obj.find_all('a','nav-item-primary')
-for text in text_list
+for text in text_list:
     print(text.get_text())
 html.close()
