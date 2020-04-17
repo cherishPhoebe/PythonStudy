831bbe9cf7feea918d82e05acac7998e2362af4c
cherishPhoebe <cherish1224@outlook.com>
2020-04-17 22:50:35 +0800

    底部飞机能够移动

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,6 +4,7 @@ class Settings():
     def __init__(self):
         """ 初始化游戏的设置 """
         # 屏幕属性设置
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (230,230,230) 
+        self.ship_speed_factor = 0.5
