c38409e65cd26d22a8208543cb8a291a18a3ec07
cherishPhoebe <cherish1224@outlook.com>
2020-04-27 21:50:08 +0800

    绘制外星人

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
 
@@ -14,12 +15,16 @@ def run_game():
     # 创建一艘飞船
     ship = Ship(ai_settings,screen)
     bullets = Group()
+    aliens = Group()
+
+    # 创建一群外星人
+    gf.create_fleet(ai_settings,screen,ship,aliens)
     
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
